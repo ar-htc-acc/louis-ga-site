6b57f6c1ec323adb81f0c5ba5bec6fdb233cde71
Louis_Huang <ar.htc.acc@gmail.com>
2017-01-16 13:39:40 +0800

    Goal.

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -2,3 +2,7 @@
 $('.blog-nav-item').removeClass('active').filter(function (index, element) {
     return $(element).attr('href') == window.location.pathname;
 }).addClass('active');
+
+$('.sign-up-button').click(function () {
+    location.href = '/tabs/signUp/';
+});
