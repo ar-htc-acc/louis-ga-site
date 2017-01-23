4fca42ba29cac19cf3ae6b3406a4f39b8efc308a
Louis_Huang <ar.htc.acc@gmail.com>
2017-01-23 14:49:19 +0800

    Custom Dimensions.

--- public/javascripts/gtm/customDimension.js
+++ public/javascripts/gtm/customDimension.js
@@ -2,4 +2,4 @@ var $container = $('div.custom-dim');
 var author = $container.data('author');
 var category = $container.data('category');
 var sport = $container.data('sport');
-if (author && category) dataLayer.push({'author': author, 'category': category, 'sport': sport});
+if (author && category && sport) dataLayer.push({'author': author, 'category': category, 'sport': sport});
