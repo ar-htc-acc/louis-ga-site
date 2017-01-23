0fffd3a4eff3f574d6bbddef3d6f3c0b1dc01aed
Louis_Huang <ar.htc.acc@gmail.com>
2017-01-23 15:07:36 +0800

    Custom Metric

--- public/javascripts/gtm/customDimension.js
+++ public/javascripts/gtm/customDimension.js
@@ -2,4 +2,5 @@ var $container = $('div.custom-dim');
 var author = $container.data('author');
 var category = $container.data('category');
 var sport = $container.data('sport');
-if (author && category && sport) dataLayer.push({'author': author, 'category': category, 'sport': sport});
+var km = $container.data('km');
+if (author && category && sport) dataLayer.push({'author': author, 'category': category, 'sport': sport, 'kilometer': km});
